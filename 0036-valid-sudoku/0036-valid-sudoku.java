4e91d9edf5f022696e0c158a4c81ca7b82184bfb
lukas-bhnk <84031517+lukas-bhnk@users.noreply.github.com>
2022-11-07 11:50:19 +0100

    Time: 107 ms (5.50%), Space: 48.5 MB (16.08%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,61 @@
+class Solution {
+
+    
+    public boolean checkValid(char[][] board, int x, int y) {
+        for (int i = 0; i < 9; i++) {
+            if (board[i][y] == board[x][y] && i != x) return false; 
+            if (board[x][i] == board[x][y] && i != y) return false;
+        }
+        for (int i = (x/3) * 3; i <= ((x/3) * 3) + 2; i++) {
+            for (int j = (y/3) * 3; j <= ((y/3) * 3) + 2; j++) {
+                if (board[i][j] == board[x][y] && (i != x || j != y)) return false; 
+            }
+        }
+        
+        return true;
+    }
+    
+    public int[] getNextFields(int x, int y) {
+        if(x < 9 && y < 8) {
+            int[] arr = { x, y+1};
+            return arr;
+        }
+        else {
+            int[] arr = { x + 1, 0};
+            return arr;
+        }
+
+    }
+    public boolean fullfillSudoku(char[][] board, int x, int y) {
+        if(x==8 && y==8) return true;
+        System.out.println(x + " " + y);
+        if (board[x][y] == '.') {
+        int[] nextFields = getNextFields(x,y);
+
+        while ((board[nextFields[0]][nextFields[1]] == ('.'))) {
+            System.out.println(nextFields[0] + " " + nextFields[1]);
+            nextFields = getNextFields(nextFields[0], nextFields[1]);
+            if (nextFields[0] == 8 && nextFields[1] == 8) return fullfillSudoku(board, nextFields[0], nextFields[1]);
+        } 
+            return fullfillSudoku(board, nextFields[0], nextFields[1]);
+        }
+        else {
+        if (!(checkValid(board, x, y))) return false;
+
+        int[] nextFields = getNextFields(x,y);
+
+        while ((board[nextFields[0]][nextFields[1]] == ('.'))) {
+            if (nextFields[0] >= 8 && nextFields[1] >= 8) break;
+            nextFields = getNextFields(nextFields[0], nextFields[1]);
+            System.out.println(nextFields[0] + " " + nextFields[1]);
+        }
+        if ((checkValid(board, x, y)) || (nextFields[0] >= 8 && nextFields[1] >= 8)){
+        return fullfillSudoku(board, nextFields[0], nextFields[1]);
+        }
+        else return false;
+        }
+    }
+    public boolean isValidSudoku(char[][] board) {
+        return fullfillSudoku(board, 0, 0);
+    }
+}
