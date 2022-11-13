aae7d53683f4296821fe696aeffd495e99f73557
Lukas <84031517+lukas-bhnk@users.noreply.github.com>
2022-11-13 11:06:35 +0100

    Time: 7 ms (10.25%), Space: 42.1 MB (8.77%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int reverse(int x) {
+        
+        if (x < 0) {
+            String s = reverse(-x, "");
+            try {
+            return Integer.valueOf("-" + s);
+            }
+            catch (NumberFormatException e) {
+                return 0;
+            }   
+       }
+        String s = reverse(x, "");
+        try {
+        return Integer.valueOf(s);}
+        catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+    
+    public String reverse(int x, String s) {
+        s += String.valueOf( x % 10);
+        if(s.length() > 2 && s.charAt(0) == '0') s = s.substring(1, s.length());
+        x = x / 10;
+        if(x != 0) s = reverse(x, s);
+        return s;
+        
+        
+    }
+}
