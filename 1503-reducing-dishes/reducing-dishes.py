f005ea405e3dad18e8ee80ff796a09b0175e09ca
Lukas <84031517+lukas-bhnk@users.noreply.github.com>
2023-10-31 14:52:16 +0100

    Time: 51 ms (55.27%) | Memory: 16.2 MB (75.02%) - LeetSync

--- 1503-reducing-dishes/reducing-dishes.py
+++ 1503-reducing-dishes/reducing-dishes.py
@@ -5,7 +5,7 @@ class Solution:
             return 0
         curMax = 0
         curSumPlus = 0
-        curSumMinus = 0
+        curSumMinus = 0 
         for i in range(len(satisfaction)):
             if satisfaction[len(satisfaction) - i - 1] >= 0:
                 curSumPlus += satisfaction[len(satisfaction) - i - 1]
